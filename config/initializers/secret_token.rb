a8839bd2db9e53e00640605c2395da0a97baed39
Sergio Almecija <sheniff@gmail.com>
2012-10-29 22:37:17 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photohub::Application.config.secret_token = '35dfad1d68b1ad21e3bf574e10409dfd4acf1c46f5b7ed8666d87eff4b4536e3f19f2dc8312483eaec280cc5f3413f735fecc357d850fd38892b7b607f5906ec'
