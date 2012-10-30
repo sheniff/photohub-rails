4794b21405a244eb20365dbdfb67bccd9ccefa71
Sergio Almecija <sheniff@gmail.com>
2012-10-30 00:45:07 -0700

    Add helpers, layouts and tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,9 +2,21 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  it "should have the content 'PhotoHub'" do
-    visit '/static_pages/home'
-    page.should have_content('PhotoHub')
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_selector('h1', text: 'PhotoHub') }
+    it { should have_selector('title', text: full_title("")) }
+    it { should_not have_selector('title', text: '| Home') }
+  end
+
+  describe "Help page" do
+    before { visit help_path }
+    
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: full_title("Help")) }
   end
 
 end
